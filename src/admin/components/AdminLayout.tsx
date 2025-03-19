45f42e23377cfeb5fd229270a09e5958ee9ac351
Bluewhalelagos <bluewhaleasian@gmail.com>
2025-03-19 20:30:04 +0000

    O

--- src/admin/components/AdminLayout.tsx
+++ src/admin/components/AdminLayout.tsx
@@ -50,7 +50,7 @@ const AdminLayout = () => {
         <div className="p-4">
         <div className="flex items-center space-x-2">
   <img 
-    src="BlueWhale-Final-logo.png" 
+    src="./BlueWhale-Final-logo.png" 
     alt="Blue Whale Logo" 
     className="h-8 w-8" 
   />
--- src/admin/pages/LoginPage.tsx
+++ src/admin/pages/LoginPage.tsx
@@ -34,7 +34,7 @@ const LoginPage = () => {
         <div className="flex justify-center mb-8">
         <div className="flex items-center space-x-2">
   <img 
-    src="BlueWhale-Final-logo.png" 
+    src="./BlueWhale-Final-logo.png" 
     alt="Blue Whale Logo" 
     className="h-8 w-8" 
   />
